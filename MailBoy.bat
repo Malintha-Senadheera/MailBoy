d356d6609447d7681c8558e66cd44dd6e93dbfae
Malintha P Senadheera <main.malinthapsenadheera@gmail.com>
2025-02-18 21:40:40 +0530

    Change color style and changing the file paths

--- MailBoy.bat
+++ MailBoy.bat
@@ -1,5 +1,5 @@
 @echo off
-title Opening a File Lesson
+title MailBoy By PMS
 
-call "C:\Users\mainm\Documents\MailBoy\MailBoy\Scripts\activate.bat"
-cmd /k python "C:\Users\mainm\Documents\MailBoy\main.py
+::call "C:\Users\ALUTECH RATHNAPURA\Documents\Malintha\MailBoy\MailBoy\Scripts\activate.bat"
+cmd /k python "C:\Users\ALUTECH RATHNAPURA\Documents\Malintha\MailBoy\main.py
